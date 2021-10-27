9d4191b7f9cf3fae7cba052e7534a675e20f17f2
Tibe Laptop <tibe.de.clercq@gmail.com>
2021-10-27 12:20:13 +0200

    Entities folder player class

---
+++ GameDevProject/Entities/Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GameDevProject.Interfaces;
+using Microsoft.Xna.Framework;
+
+namespace GameDevProject.Entities
+{
+    class Player : IMovable
+    {
+        #region IMovable implementation
+        public Vector2 Position { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public Vector2 Speed { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public IInputReader InputReader { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        #endregion
+    }
+}
