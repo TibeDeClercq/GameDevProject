d1fd3af2192bf3a2e427bb544c7e1fd558c1a061
Evert <bekaertevert@gmail.com>
2021-10-27 12:24:55 +0200

    Animation classes toegevoegd + temp spritesheet

---
+++ GameDevProject/Entities/Animations/AnimationFrame.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameDevProject.Entities.Animations
+{
+    class AnimationFrame
+    {
+        public Rectangle SourceRectangle { get; set; }
+
+        public AnimationFrame(Rectangle sourceRectangle)
+        {
+            SourceRectangle = sourceRectangle;
+        }
+
+    }
+}
