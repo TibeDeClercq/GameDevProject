6efe81477a89449093fa2e37191cb9a40b9e307c
Tibe Laptop <tibe.de.clercq@gmail.com>
2021-10-27 12:33:31 +0200

    Enrity class methods

--- GameDevProject/Entities/Entity.cs
+++ GameDevProject/Entities/Entity.cs
@@ -1,19 +1,31 @@
-﻿using GameDevProject.Entities.Animations;
-using Microsoft.Xna.Framework;
+﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
+using GameDevProject.Interfaces;
+using GameDevProject.Entities.Animations;
+
 namespace GameDevProject.Entities
 {
-    class Entity
+    abstract class Entity
     {
         private Texture2D texture;
         private Animation animation;
-
+        private IInputReader inputReader;
         public Vector2 Speed { get; set; }
         public Vector2 Position { get; set; }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, Position, animation.CurrentFrame.SourceRectangle, Color.White);
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            animation.Update(gameTime);
+        }
 
     }
 }
