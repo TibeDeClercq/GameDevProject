1b0150af5375fda44116ec848e10593274274355
EvertBt <67715093+EvertBt@users.noreply.github.com>
2021-11-30 15:32:20 +0100

    physics manager update

--- GameDevProject/Managers/PhysicsManager.cs
+++ GameDevProject/Managers/PhysicsManager.cs
@@ -8,21 +8,25 @@ namespace GameDevProject.Managers
 {
     class PhysicsManager
     {
+        private bool canJump = true;
+        private Vector2 gravity = new Vector2(0, 5);
+        //https://stackoverflow.com/questions/43024877/xna-monogame-platformer-jumping-physics-and-collider-issue
+
         public static void MoveRight(IMovable movable)
         {
-            var distance = new Vector2(1,0) * movable.Speed;
+            var distance = new Vector2(1,0) * movable.MaxVelocity;
             movable.Position += distance;
         }
 
         public static void MoveLeft(IMovable movable)
         {
-            var distance = new Vector2(-1,0) * movable.Speed;
+            var distance = new Vector2(-1,0) * movable.MaxVelocity;
             movable.Position += distance;
         }
 
         public static void Jump(IMovable movable)
         {
-            var distance = new Vector2(0, -3);
+            var distance = new Vector2(0, -1) * movable.MaxJumpHeight;
             movable.Position += distance;
         }
 
