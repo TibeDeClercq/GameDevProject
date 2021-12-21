052882ce6bbe7cad4a56712931d9fef4728a6ec9
TibeDeClercq <79404302+TibeDeClercq@users.noreply.github.com>
2021-12-21 12:33:48 +0100

    game1 name conventions

--- GameDevProject/Game1.cs
+++ GameDevProject/Game1.cs
@@ -12,35 +12,35 @@ namespace GameDevProject
 {
     public class Game1 : Game
     {
-        private RenderTarget2D _gameRenderTarget;
+        private RenderTarget2D gameRenderTarget;
         private int scale = 3;
 
-        private GraphicsDeviceManager _graphics;
-        private SpriteBatch _spriteBatch;
+        private GraphicsDeviceManager graphics;
+        private SpriteBatch spriteBatch;
 
         private List<Entity> entities;
         //private Player player;
-        private List<Texture2D> _playerTextures;
+        private List<Texture2D> playerTextures;
 
         private World world1;
-        private Texture2D _worldTileset;
+        private Texture2D worldTileset;
         public Game1()
         {
-            _graphics = new GraphicsDeviceManager(this);
+            this.graphics = new GraphicsDeviceManager(this);
 
-            Content.RootDirectory = "Content";
-            IsMouseVisible = true;
+            this.Content.RootDirectory = "Content";
+            this.IsMouseVisible = true;
 
-            _graphics.IsFullScreen = false;
+            this.graphics.IsFullScreen = false;
         }
 
         protected override void Initialize()
         {
             base.Initialize();
 
-            AddEntities();
-            AddWorld();
-            SetRenderer();
+            this.AddEntities();
+            this.AddWorld();
+            this.SetRenderer();
 
             PhysicsManager.entities = this.entities;
             //PhysicsManager.tiles = this.world1. GETTILES
@@ -48,8 +48,8 @@ namespace GameDevProject
 
         protected override void LoadContent()
         {
-            AddSpriteBatch();
-            AddTextures();
+            this.AddSpriteBatch();
+            this.AddTextures();
             // TODO: use this.Content to load your game content here
         }
 
@@ -57,11 +57,11 @@ namespace GameDevProject
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
             {
-                Exit();
+                this.Exit();
             }
 
             //Update entities
-            UpdateEntities(gameTime);
+            this.UpdateEntities(gameTime);
 
             base.Update(gameTime);
         }
@@ -69,18 +69,18 @@ namespace GameDevProject
         protected override void Draw(GameTime gameTime)
         {
             //Draw all objects to a frame
-            DrawToFrame();
+            this.DrawToFrame();
             //Draw frame to window => rescale
-            DrawToScreen();
+            this.DrawToScreen();
             base.Draw(gameTime);
         }
 
         #region Initialize
         private void AddEntities()
         {
-            entities = new List<Entity>();
+            this.entities = new List<Entity>();
 
-            entities.Add(new Player(_playerTextures, new KeyboardReader()));
+            this.entities.Add(new Player(this.playerTextures, new KeyboardReader()));
         }
 
         private void AddWorld()
@@ -96,54 +96,54 @@ namespace GameDevProject
                                 { "E3", "E3", "E3", "E3", "E3", "E3","E3", "E3", "E3", "E3", "E3", "E3"}
                              };
 
-            world1 = new World(_worldTileset, test);
+            this.world1 = new World(this.worldTileset, test);
         }
 
         private void SetRenderer()
         {
-            _gameRenderTarget = new RenderTarget2D(GraphicsDevice, world1.GetWorldWidth(), world1.GetWorldHeight());
+            this.gameRenderTarget = new RenderTarget2D(this.GraphicsDevice, this.world1.GetWorldWidth(), this.world1.GetWorldHeight());
 
-            _graphics.PreferredBackBufferHeight = scale * world1.GetWorldHeight(); //getWorldHeight
-            _graphics.PreferredBackBufferWidth = scale * world1.GetWorldWidth(); //getWorldWidth
-            _graphics.ApplyChanges();
+            this.graphics.PreferredBackBufferHeight = this.scale * this.world1.GetWorldHeight(); //getWorldHeight
+            this.graphics.PreferredBackBufferWidth = this.scale * this.world1.GetWorldWidth(); //getWorldWidth
+            this.graphics.ApplyChanges();
         }
         #endregion
 
         #region LoadContent
         private void AddSpriteBatch()
         {
-            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            this.spriteBatch = new SpriteBatch(this.GraphicsDevice);
         }
 
         private void AddTextures()
         {
-            AddPlayerTextures();
-            AddWorldTextures();
+            this.AddPlayerTextures();
+            this.AddWorldTextures();
         }
 
         private void AddPlayerTextures()
         {
-            _playerTextures = new List<Texture2D>();
+            this.playerTextures = new List<Texture2D>();
 
-            _playerTextures.Add(Content.Load<Texture2D>("SpritesheetsPlayer/PlayerIdle"));
-            _playerTextures.Add(Content.Load<Texture2D>("SpritesheetsPlayer/PlayerWalk"));
-            _playerTextures.Add(Content.Load<Texture2D>("SpritesheetsPlayer/PlayerJump"));
-            _playerTextures.Add(Content.Load<Texture2D>("SpritesheetsPlayer/PlayerSpin"));
-            _playerTextures.Add(Content.Load<Texture2D>("SpritesheetsPlayer/PlayerSleep"));
+            this.playerTextures.Add(this.Content.Load<Texture2D>("SpritesheetsPlayer/PlayerIdle"));
+            this.playerTextures.Add(this.Content.Load<Texture2D>("SpritesheetsPlayer/PlayerWalk"));
+            this.playerTextures.Add(this.Content.Load<Texture2D>("SpritesheetsPlayer/PlayerJump"));
+            this.playerTextures.Add(this.Content.Load<Texture2D>("SpritesheetsPlayer/PlayerSpin"));
+            this.playerTextures.Add(this.Content.Load<Texture2D>("SpritesheetsPlayer/PlayerSleep"));
         }
 
         private void AddWorldTextures()
         {
-            _worldTileset = Content.Load<Texture2D>("TileSetsWorld/TilesetWorld");
+            this.worldTileset = this.Content.Load<Texture2D>("TileSetsWorld/TilesetWorld");
         }
         #endregion
 
         #region Update
         private void UpdateEntities(GameTime gameTime)
         {
-            foreach (Entity entity in entities)
+            foreach (Entity entity in this.entities)
             {
-                entity.Update(gameTime, world1);
+                entity.Update(gameTime, this.world1);
             }
         }
         #endregion
@@ -151,29 +151,29 @@ namespace GameDevProject
         #region Draw
         private void DrawToFrame()
         {
-            GraphicsDevice.SetRenderTarget(_gameRenderTarget);
-            GraphicsDevice.Clear(Color.CornflowerBlue);
-            _spriteBatch.Begin();
+            this.GraphicsDevice.SetRenderTarget(this.gameRenderTarget);
+            this.GraphicsDevice.Clear(Color.CornflowerBlue);
+            this.spriteBatch.Begin();
             //Draw world
-            world1.Draw(_spriteBatch);
+            this.world1.Draw(this.spriteBatch);
             //Draw entities
-            DrawEntities();
-            _spriteBatch.End();
+            this.DrawEntities();
+            this.spriteBatch.End();
         }
 
         private void DrawToScreen()
         {
-            GraphicsDevice.SetRenderTarget(null);
-            _spriteBatch.Begin(samplerState: SamplerState.PointClamp);
-            _spriteBatch.Draw(_gameRenderTarget, new Rectangle(0, 0, scale * world1.GetWorldWidth(), scale * world1.GetWorldHeight()), Color.White);
-            _spriteBatch.End();
+            this.GraphicsDevice.SetRenderTarget(null);
+            this.spriteBatch.Begin(samplerState: SamplerState.PointClamp);
+            this.spriteBatch.Draw(this.gameRenderTarget, new Rectangle(0, 0, this.scale * this.world1.GetWorldWidth(), this.scale * this.world1.GetWorldHeight()), Color.White);
+            this.spriteBatch.End();
         }
 
         private void DrawEntities()
         {
-            foreach (Entity entity in entities)
+            foreach (Entity entity in this.entities)
             {
-                entity.Draw(_spriteBatch);
+                entity.Draw(this.spriteBatch);
             }
         }
         #endregion
